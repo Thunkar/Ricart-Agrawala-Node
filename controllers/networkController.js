51c961ad71e4b31ec25a7dbf12c43d30ea15c20d
Gregorio Juliana <gregojquiros@gmail.com>
2016-05-21 13:10:23 +0200

    Final fixes and algorithm implementation

--- controllers/networkController.js
+++ controllers/networkController.js
@@ -26,8 +26,16 @@ var getParent = function () {
 };
 
 var pendingElection = true;
-var timers = [];
+var electionTimers = [];
+var criticalSectionTimers = [];
+var criticalSectionRequestTimers = [];
+var wantedTimeoutTimers = [];
+var lastRequested = new Date();
 var coordinator = "-1";
+var state = "RELEASED";
+var answered = {};
+var messages = {};
+var offset = 0;
 
 //Client 
 
@@ -45,6 +53,7 @@ var configureClient = function () {
         }
     });
 
+
     var serverLocation = getParent();
     server = { socket: clientSocket };
     config.serverPort = serverLocation.split(":")[1];
@@ -55,11 +64,16 @@ var configureClient = function () {
         clientSocket.write(JSON.stringify({ type: "HANDSHAKE", from: config.pid, timestamp: new Date(), ttl: 1 }) + "\r\n");
     });
 
-    clientSocket.on('data', function (d) {
+    clientSocket.on('data', function (data) {
         try {
-            var data = JSON.parse(d);
-        } catch (err) { }
-        processMessage(server, data);
+            var splitted = data.toString().match(/[^\r\n]+/g);
+            splitted.forEach(function (line) {
+                var json = JSON.parse(line);
+                processMessage(server, json);
+            });
+        } catch (err) {
+            if (err) return systemLogger.error(err.message);
+        }
     });
 
     clientSocket.on('error', function (err) {
@@ -89,10 +103,9 @@ var configureServer = function () {
 
         socket.on('data', function (data) {
             try {
-                var splitted = data.toString().split("\r\n");
-                splitted.forEach(function (message) {
-                    if (!message || message == "") return;
-                    var json = JSON.parse(message);
+                var splitted = data.toString().match(/[^\r\n]+/g);
+                splitted.forEach(function (line) {
+                    var json = JSON.parse(line);
                     processMessage(client, json);
                 });
             } catch (err) {
@@ -115,7 +128,6 @@ var configureServer = function () {
             } catch (err) { }
             election();
         });
-
     }).listen(process.env.PORT || config.port, function () {
         configureClient();
     });
@@ -138,7 +150,7 @@ var send = function (message, excluded) {
 
 
 var status = function () {
-    var statusObject = { me: config.pid, server: server.pid, coordinator: coordinator, clients: [] };
+    var statusObject = { me: config.pid, server: server.pid, coordinator: coordinator, state: state, clients: [] };
     for (node in clients) {
         statusObject.clients.push(node);
     }
@@ -146,35 +158,78 @@ var status = function () {
 }
 
 var election = function () {
-    systemLogger.info("Starting election");
+    systemLogger.info("Starting election, forcing out of critical section");
+    state = "RELEASED";
     coordinator = "-1";
     pendingElection = true;
-    timers.forEach(function (timer) {
+    electionTimers.concat(criticalSectionTimers).concat(criticalSectionRequestTimers).concat(wantedTimeoutTimers).forEach(function (timer) {
         clearTimeout(timer);
     });
     for (var i = 0; i < config.pid; i++) {
         send({ type: "ELECTION", from: config.pid, to: i + "", timestamp: new Date(), ttl: config.nodes.length })
     }
-    timers.push(setTimeout(function () {
+    electionTimers.push(setTimeout(function () {
         if (!pendingElection) return;
         send({ type: "COORDINATOR", from: config.pid, to: "-1", timestamp: new Date(), ttl: config.nodes.length });
         pendingElection = false;
         coordinator = config.pid;
         systemLogger.info("No one answered, I'm the coordinator");
+        offset = 0;
+        requestCriticalSection(0);
     }, config.electionTimeout))
 }
 
+var leaveCriticalSection = function () {
+    systemLogger.info("Leaving critical section");
+    state = "RELEASED";
+    for (var dest in messages) {
+        send({ type: "ACCEPT", from: config.pid, to: dest, timestamp: new Date(), ttl: config.nodes.length })
+    }
+    messages = {};
+}
+
+var enterCriticalSection = function () {
+    if (pendingElection) return;
+    systemLogger.info("Entering critical section");
+    answered = {};
+    state = "HELD";
+    criticalSectionTimers.concat(criticalSectionRequestTimers).concat(wantedTimeoutTimers).forEach(function (timer) {
+        clearTimeout(timer);
+    });
+    criticalSectionTimers.push(setTimeout(function () {
+        leaveCriticalSection();
+        requestCriticalSection(Math.floor(Math.random() * config.maxCriticalSectionDelay));
+    }, config.criticalSectionTimeout));
+}
+
+var requestCriticalSection = function (delay) {
+    if (pendingElection) return;
+    criticalSectionTimers.concat(criticalSectionRequestTimers).concat(wantedTimeoutTimers).forEach(function (timer) {
+        clearTimeout(timer);
+    });
+    criticalSectionRequestTimers.push(setTimeout(function () {
+        systemLogger.info("Requested critical section access");
+        answered = {};
+        state = "WANTED";
+        lastRequested = new Date(new Date() - offset);
+        send({ type: "WANTED", from: config.pid, to: "-1", timestamp: lastRequested, ttl: config.nodes.length })
+        wantedTimeoutTimers.push(setTimeout(function () {
+            systemLogger.warn("Token lost, starting election");
+            election();
+        }, 15000 * config.nodes.length));
+    }, delay));
+}
 
 var processMessage = function (node, message) {
     if (!message || message.from == config.pid || message.ttl < 1) return;
     else message.ttl--;
-    systemLogger.debug(message);
     if (message.type != "HANDSHAKE" && message.to != config.pid) {
         systemLogger.debug("Received a message for another node, passing it along...");
         send(message, node.pid);
         if (message.to != "-1")
             return;
     }
+    systemLogger.debug(message);
     switch (message.type) {
         case "HANDSHAKE": {
             node.pid = message.from;
@@ -190,7 +245,7 @@ var processMessage = function (node, message) {
         }
         case "ACKHANDSHAKE": {
             if (config.nodes.length > 2 && clients[message.from]) {
-                systemLogger.warn("Server loop detected");
+                systemLogger.warn("Loop detected");
                 server.socket.end();
                 node.socket.end();
             } else {
@@ -200,7 +255,7 @@ var processMessage = function (node, message) {
             break;
         }
         case "ELECTION": {
-            if (pendingElection) return;
+            if (message.from < config.pid) return;
             send({ type: "ANSWER", from: config.pid, to: message.from, timestamp: new Date(), ttl: config.nodes.length });
             election();
             break;
@@ -215,9 +270,27 @@ var processMessage = function (node, message) {
                 election();
                 return;
             }
-            systemLogger.info("Node: " + message.from + " is the coordinator");
+            systemLogger.info("Node: " + message.from + " is the coordinator, forcing myself out of the critical section");
+            offset = new Date() - new Date(message.timestamp);
             coordinator = message.from;
             pendingElection = false;
+            leaveCriticalSection();
+            requestCriticalSection(Math.floor(Math.random() * config.maxCriticalSectionDelay));
+            break;
+        }
+        case "WANTED": {
+            if (state == "RELEASED" || lastRequested > message.timestamp) {
+                send({ type: "ACCEPT", from: config.pid, to: message.from, timestamp: new Date(), ttl: config.nodes.length });
+            } else {
+                messages[message.from] = message;
+            }
+            break;
+        }
+        case "ACCEPT": {
+            if (state != "WANTED") return;
+            answered[message.from] = 1;
+            if (Object.keys(answered).length == config.nodes.length - 1)
+                enterCriticalSection();
             break;
         }
         default: break;
